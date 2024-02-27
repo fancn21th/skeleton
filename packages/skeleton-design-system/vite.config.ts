2811b1a51285f57b8387e1831443da5ad306db84
fancn21th <fancn21th@gmail.com>
2024-02-27 13:50:52 +0800

    📦 NEW: a test component

--- packages/skeleton-design-system/vite.config.ts
+++ packages/skeleton-design-system/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/components"),
+      name: "skeleton-design-system",
+      fileName: (format) => `skeleton-design-system.${format}.js`,
+    },
+  },
+});

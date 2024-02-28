5f52d16f82033d8f0c27fe606cc42040d56ee0ab
fancn21th <fancn21th@gmail.com>
2024-02-28 14:49:22 +0800

    👌 IMPROVE: 支持 ts

--- packages/skeleton-design-system/src/components/Test/Test.tsx
+++ packages/skeleton-design-system/src/components/Test/Test.tsx
@@ -1,5 +1,3 @@
-function Test() {
+export function Test() {
   return <div>Test</div>;
 }
-
-export default Test;
--- packages/skeleton-design-system/vite.config.ts
+++ packages/skeleton-design-system/vite.config.ts
@@ -1,15 +1,32 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ["src/components"],
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/components"),
       name: "skeleton-design-system",
       fileName: (format) => `skeleton-design-system.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"], // make sure to exclude react from the bundle
+      output: {
+        globals: {
+          // provide global variable names for the external imports
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "jsxRuntime",
+        },
+      },
     },
   },
 });

c1c0e3098aca12a0c8a255d5347a69470a31efa2
fancn21th <fancn21th@gmail.com>
2024-02-28 17:30:17 +0800

    📦 NEW: 可以通过 npm link 来使用 Test 组件

--- packages/skeleton-design-system/src/components/Test/Test.tsx
+++ packages/skeleton-design-system/src/components/Test/Test.tsx
@@ -1,3 +1,5 @@
+import styles from "./Test.module.css";
+
 export function Test() {
-  return <div>Test</div>;
+  return <div className={styles.div}>Test</div>;
 }
--- packages/skeleton-design-system/vite.config.ts
+++ packages/skeleton-design-system/vite.config.ts
@@ -2,12 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(), // css
     dts({
+      // d.ts
       include: ["src/components"],
     }),
   ],
@@ -15,7 +18,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "src/components"),
       name: "skeleton-design-system",
-      fileName: (format) => `skeleton-design-system.${format}.js`,
+      formats: ["cjs", "es"],
     },
     rollupOptions: {
       external: ["react", "react-dom", "react/jsx-runtime"], // make sure to exclude react from the bundle

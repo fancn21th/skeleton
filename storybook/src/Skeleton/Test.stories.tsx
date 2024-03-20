087bae830cdc406b4b8d3654b45fc1da41b1d433
fancn21th <fancn21th@gmail.com>
2024-03-20 15:54:12 +0800

    👌 IMPROVE: a new structure for skeleton components

---
+++ storybook/src/Skeleton/Test.stories.tsx
@@ -0,0 +1,10 @@
+import { Test } from "@fancn21th/skeleton-design-system";
+
+export default {
+  title: "Skeleton/Test",
+  component: Test,
+};
+
+export const Default = () => {
+  return <Test>Hello</Test>;
+};

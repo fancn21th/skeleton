f0e4fc1ef5cf9d7cf79696430c2aa4840d7a3b2b
fancn21th <fancn21th@gmail.com>
2024-03-20 12:03:02 +0800

    📦 NEW: 第一个 sb 故事

--- packages/skeleton-design-system/src/components/Test/Test.tsx
+++ packages/skeleton-design-system/src/components/Test/Test.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import styles from "./Test.module.css";
 
-export const Test: React.FC = () => {
-  return <div className={styles.div}>Test</div>;
+interface TestProps {
+  children: React.ReactNode;
+}
+
+export const Test = ({ children, ...props }: TestProps) => {
+  return (
+    <div className={styles.div} {...props}>
+      {children}
+    </div>
+  );
 };

1dc06601c099215879f1523f9935a399f937434c
fancn21th <fancn21th@gmail.com>
2024-03-21 13:44:53 +0800

    👌 IMPROVE: write the story in ts style

--- storybook/src/Skeleton/Test.stories.tsx
+++ storybook/src/Skeleton/Test.stories.tsx
@@ -1,10 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
 import { Test } from "@fancn21th/skeleton-design-system";
 
-export default {
+const meta = {
   title: "Skeleton/Test",
   component: Test,
+} satisfies Meta<typeof Test>;
+
+export default meta;
+
+type Story = StoryObj<typeof Test>;
+
+// 故事 Default
+export const Default: Story = {
+  args: {
+    children: "Test",
+  },
 };
 
-export const Default = () => {
-  return <Test>Hello</Test>;
+// 故事 Another
+export const Another: Story = {
+  args: {
+    children: "Test2",
+  },
 };
